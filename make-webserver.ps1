b9b813f09c52ef2dc648a138d56075f538dc82da
Michael Yeaney <michael.yeaney@gmail.com>
2017-02-18 17:55:56 -0500

    Cleaning up filenames

---
+++ make-webserver.ps1
@@ -0,0 +1,2 @@
+fsc .\webserver.fs --reference:.\Packages\Newtonsoft.Json.9.0.1\lib\net45\Newtonsoft.Json.dll --out:.\bin\webserver.exe
+copy .\Packages\Newtonsoft.Json.9.0.1\lib\net45\Newtonsoft.Json.dll .\bin\
--- webserver.fs
+++ webserver.fs
@@ -7,6 +7,11 @@ open Newtonsoft.Json
 let port = 5555
 let host = String.Format("http://localhost:{0}/", port)
 
+let WriteLogMessage (msg:string) =
+    let timeStamp = DateTime.Now.ToString("G")
+    let formattedMsg = String.Format("[{0}] INFO:: {1}\n", timeStamp, msg)
+    File.AppendAllText(".\\log.txt", formattedMsg, Encoding.UTF8)
+
 let listener handler =
     let hl = new HttpListener()
     hl.Prefixes.Add host
@@ -33,13 +38,13 @@ listener (fun req resp ->
         let txt = Encoding.UTF8.GetBytes(output req)
         resp.ContentType <- "text/plain"
         resp.StatusCode <- status
-        Console.WriteLine("Tried to set status {0}", resp.StatusCode)
+        WriteLogMessage(String.Format("{0} - {1}", req.RawUrl, resp.StatusCode))
         resp.OutputStream.Write(txt, 0, txt.Length)
         resp.OutputStream.Close()
         resp.Close()
     }
 )
 
-Console.WriteLine("Started server on port {0}", port)
+WriteLogMessage(String.Format("Started server on port {0}", port))
 Console.Read() |> ignore
-Console.WriteLine("Shutting down...")
+WriteLogMessage("Shutting down...")
