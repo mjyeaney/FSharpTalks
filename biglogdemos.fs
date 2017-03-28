8f3faf3936cdb7571acbebfab49450b995c9e17b
Michael Yeaney <michael.yeaney@gmail.com>
2017-03-28 10:32:43 -0400

    Updated big log samples with proper IDisposable pattern

--- biglogdemos.fs
+++ biglogdemos.fs
@@ -2,15 +2,17 @@ open System.IO;;
 
 let readData () =
     seq {
-        let reader = new StreamReader("biglog.txt")
+        use reader = new StreamReader("biglog.txt")
         while (not reader.EndOfStream) do
             yield reader.ReadLine() |> float
-        reader.Dispose()
     };;
 
-readData() |> Seq.average
+#time;;
+readData() |> Seq.average;;
 
 let cachedData =
     readData() |> Seq.cache;;
 
 cachedData |> Seq.average;;
+cachedData |> Seq.average;;
+#time;;
